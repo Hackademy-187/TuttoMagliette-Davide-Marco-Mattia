7b00313574e4c6b16c881b218b9819419abc4cde
marcocarbonara2707-a11y <marcocarbonara2707@gmail.com>
2026-05-05 17:02:21 +0200

    prova card

--- resources/js/SerieA.js
+++ resources/js/SerieA.js
@@ -9,6 +9,27 @@
 
 console. log('Funziona');
 
-fetch(' .. /annunci. json')
+fetch(' ../SerieA.json')
 . then ( response => response. json ())
-.then (data => console. log (data) )
+.then (data => {
+
+
+let containercards = document.querySelector(`#container-card`)
+
+// Creare le CARD di ANNUNCI
+data.sort((a, b) => b.price - a.price).forEach((annuncio) => {
+  let div = document.createElement('div');
+  div.classList.add("col-12", "col-md-3");
+  div.innerHTML = `
+    <div class="card card-prodotto text-center p-3">
+      <img src="${annuncio.url}" class="img-fluid mb-3" />
+      <h6 class="text-primary-emphasis">${annuncio.name}</h6>
+      <span class="prezzo-vecchio">${annuncio.price} €</span>
+      <h4>${annuncio.category}</h4>
+      <h4>${annuncio.location}</h4>
+      <button class="btnCompra mt-3" data-bs-target="#modalInter">Compra</button>
+    </div>
+  `;
+  containercards.appendChild(div);
+});
+});
