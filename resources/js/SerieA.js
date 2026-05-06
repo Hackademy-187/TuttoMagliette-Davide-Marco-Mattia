d9b5c7c30c87eadfa56e5861e88388d4b3169c23
davide-sketch <120529757+davide-sketch@users.noreply.github.com>
2026-05-06 17:37:44 +0200

    aggiunta filtro

--- resources/js/SerieA.js
+++ resources/js/SerieA.js
@@ -7,31 +7,8 @@
 
 console.log("Funziona");
 
-let allData = [];
-let containercards = document.querySelector("#containerCards");
 
-// 🔹 FUNZIONE PER CREARE CARDS (RIUTILIZZABILE)
-function createCards(array) {
-    containercards.innerHTML = ""; // pulisce prima
 
-    array.forEach((annuncio) => {
-        let div = document.createElement("div");
-        div.classList.add("col-12", "col-md-3");
-
-        div.innerHTML = `
-            <div class="card card-prodotto text-center p-3 h-100 shadow">
-                <img src="${annuncio.url}" class="img-fluid mb-3" />
-                <h6 class="text-primary-emphasis">${annuncio.name}</h6>
-                <span class="text-decoration-line-through text-muted">${annuncio.price} €</span>
-                <h5 class="fw-bold">${annuncio.category}</h5>
-                <p>${annuncio.location}</p>
-                <button class="btn btn-dark mt-auto">Compra</button>
-            </div>
-        `;
-
-        containercards.appendChild(div);
-    });
-}
 
 
 // 🔹 FETCH DATI
@@ -39,41 +16,89 @@ fetch("../SerieA.json")
     .then((response) => response.json())
     .then((data) => {
 
-        // SALVI I DATI GLOBALI
-        allData = data;
+
 
         // ORDINA
-        allData.sort((a, b) => b.price - a.price);
+        data.sort((a, b) => b.price - a.price);
+        let containercards = document.querySelector("#containerCards");
+        let containerRadio = document.querySelector('#containerRadio')
+        // 🔹 FUNZIONE PER CREARE CARDS (RIUTILIZZABILE)
+        function createCards(array) {
+            containercards.innerHTML = ""; // pulisce prima
+
+            array.forEach((annuncio) => {
+                let div = document.createElement("div");
+                div.classList.add("col-12", "col-md-3", "mb-5");
+
+                div.innerHTML = `
+            <div class= "card card-prodotto text-center p-3 h-100 shadow ">
+                <img src="${annuncio.url}" class="img-fluid mb-3" />
+                <h6 class="text-white">${annuncio.name}</h6>
+                <span class="text-decoration-line-through text-white">${annuncio.price}€  </span>
+                <span class="text-white">${annuncio.discountedPrice} €</span>
+                <h5 class="text-white text-fw-bold">${annuncio.category}</h5>
+                <p class="text-white">${annuncio.location}</p>
+                <button class="btn btn-dark mt-auto">Compra</button>
+            </div>
+        `;
+
+                containercards.appendChild(div);
+            });
+        }
 
         // MOSTRA
-        createCards(allData);
+        createCards(data);
+
+        let categories = []
+        data.forEach((annuncio) => {
+            if (!categories.includes(annuncio.category)) {
+                categories.push(annuncio.category)
+
+            }
+        })
+        categories.forEach(category => {
+            let div = document.createElement('div')
+            div.classList.add('form-check')
+            div.innerHTML = `
+             <input class="form-check-input" type="radio" name="categories" id="${category}"/>
+                                    <label class="form-check-label" for="All">
+                                        ${category}
+                                    </label> 
+            `
+            containerRadio.appendChild(div)
+        })
+        // 🔹 FILTRO CATEGORIA
+        function categoryByFilter(category) {
+
+            if (category === "All") {
+                createCards(data);
+            } else {
+
+                let filtered = data.filter(
+                    (annuncio) => annuncio.category === category
+                );
+                console.log(filtered);
+
+                createCards(filtered);
+            }
+        }
+
+
+        // 🔹 EVENT LISTENER RADIO
+        let radioCategories = document.querySelectorAll(".form-check-input");
+
+        radioCategories.forEach((radio) => {
+            radio.addEventListener("click", () => {
+
+                let categoria = radio.id
+                console.log(categoria);
+
+                categoryByFilter(categoria);
+            });
+        });
     });
 
 
-// 🔹 FILTRO CATEGORIA
-function categoryByFilter(category) {
-
-    if (category === "All") {
-        createCards(allData);
-    } else {
-
-        let filtered = allData.filter(
-            (annuncio) => annuncio.location === category
-        );
-
-        createCards(filtered);
-    }
-}
 
 
-// 🔹 EVENT LISTENER RADIO
-let radioCategories = document.querySelectorAll(".form-check-input");
 
-radioCategories.forEach((radio) => {
-    radio.addEventListener("click", () => {
-
-        let categoria = radio.nextElementSibling.textContent.trim();
-
-        categoryByFilter(categoria);
-    });
-});
